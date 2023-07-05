1d413191b0a698952db53552f93d0d747da6820a
imperator-goose <glookeemaska133@gmail.com>
2023-07-05 17:16:51 +0300

    vsyo capec

--- src/Field.java
+++ src/Field.java
@@ -1,6 +1,19 @@
+import java.util.*;
 import java.util.Scanner;
 public class Field {
-    static char[][] field = new char[3][3];
+
+    private static char[][] field = new char[3][3];
+    private static final char Field_X= 'x';
+    private static final char Field_O= '0';
+    private static final char Field_Null= '-';
+    Field(){
+        fill();
+    };
+    private static void fill (){
+        for(char[] charElement : field){
+            Arrays.fill(charElement, Field_Null);
+        }
+    }
 }
 
 
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,60 +1,6 @@
 import java.util.Scanner;
+class GameLogic {
+        public static void start(){
 
-public class GameLogic {
-        String coordinates;
-        char coordinateX,coordinateY;
-        byte moveCounter=1;
-
-
-        public byte getMoveCounter(){
-            return moveCounter;
         }
-
-        public char[][] update(char gridOfGame[][]) {
-
-            if (moveCounter%2==0){
-                System.out.println("\tХод №"+ moveCounter+". Крестики, ваша очередь! Введите координаты хода (пример:a2): ");} else {
-                System.out.println("\tХод №"+ moveCounter+". Нолики, ваша очередь! Введите координаты хода (пример:a2): ");
-            }
-            Scanner scannerOfPlayerMove = new Scanner(System.in);
-
-
-            coordinates=scannerOfPlayerMove.next();
-
-            if (coordinates.length()!=2) {
-                System.out.println("Вы ввели не верный формат хода. Попробуйте снова (пример: a2)");
-                return gridOfGame;
-            }
-
-            coordinateX=coordinates.charAt(0);
-            coordinateY=coordinates.charAt(1);
-
-            int x=0,y=0;
-            switch (coordinateX){
-                case 'a':x=0;break;
-                case 'b':x=1;break;
-                case 'c':x=2;break;
-                default:System.out.println("Вы ввели не верный формат хода. Попробуйте снова (пример: a2)\n");
-                    return gridOfGame;
-            }
-            switch (coordinateY){
-                case '1':y=0;break;
-                case '2':y=1;break;
-                case '3':y=2;break;
-                default:System.out.printf("Вы ввели не верный формат хода. Попробуйте снова (пример: a2)\n");
-                    return gridOfGame;
-            }
-
-            if (gridOfGame[x][y]=='-') {
-                if ((moveCounter%2==0)&&(gridOfGame[x][y]!='x')&&(gridOfGame[x][y]!='o')){
-                    gridOfGame[x][y]='x';moveCounter++;
-                } else {
-                    gridOfGame[x][y]='o';moveCounter++;}
-            } else {
-                System.out.println("Клетка занята,попробуйте снова \t");
-            }
-
-            return gridOfGame;
-        }
-    }
-
+}
